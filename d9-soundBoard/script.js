5285ab3b228587bf68c9b00753ddbdcf35c989ff
example <alisayar0107@gmail.com>
2021-05-20 09:05:52 +0300

    buttons styled

--- d9-soundBoard/script.js
+++ d9-soundBoard/script.js
@@ -1,8 +1,9 @@
-const labels = document.querySelectorAll('.form-control label');
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
-labels.forEach((label) => {
-  label.innerHTML = label.innerText
-    .split('')
-    .map((letter, i) => `<span>${letter}</span>`)
-    .join('');
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.innerText = sound;
+  document.getElementById('buttons').appendChild(btn);
+  3;
 });
