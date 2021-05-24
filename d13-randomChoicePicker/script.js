d2ebb5385a030497a5404bbbfc5149f96a89a88f
example <alisayar0107@gmail.com>
2021-05-24 10:57:22 +0300

    highlighting animation added, functions created

--- d13-randomChoicePicker/script.js
+++ d13-randomChoicePicker/script.js
@@ -5,8 +5,15 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value);
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+    randomSelect();
+  }
 });
 
+// Create tags and put them into DOM.
 function createTags(input) {
   // filter blank spaces and multiple comma
   const tags = input
@@ -21,4 +28,34 @@ function createTags(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+//Runs when user hit the enter, animation and select.
+function randomSelect() {
+  //Number of times it will highlight before stoping
+  const times = 30;
+  //Interval to higlight every 100ms
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+    setTimeout(() => {
+      unhighlightTag(randomTag);
+    }, 100);
+  }, 100);
+}
+
+// To highlight randomly
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+// To add highlight class
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+// To remove highlight class
+function unhighlightTag(tag) {
+  tag.classList.remove('highlight');
 }
