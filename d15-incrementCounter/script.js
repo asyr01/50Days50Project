37b25b6718e65c37c7860466a988121f5feea188
example <alisayar0107@gmail.com>
2021-05-26 16:39:06 +0300

    script completed, project completed

--- d15-incrementCounter/script.js
+++ d15-incrementCounter/script.js
@@ -7,11 +7,14 @@ counters.forEach((counter) => {
     const target = Number(counter.getAttribute('data-target'));
     const c = Number(counter.innerText);
 
+    // you can change the speed of increment here
     const increment = target / 100;
 
     if (c < target) {
       counter.innerText = `${Math.ceil(c + increment)}`;
       setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
     }
   };
 
