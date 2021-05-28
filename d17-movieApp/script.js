d2a6591b3a3fd3d6b77e3f84e2e948cfe0e00d6b
example <alisayar0107@gmail.com>
2021-05-28 14:11:50 +0300

    Script completed, project completed

--- d17-movieApp/script.js
+++ d17-movieApp/script.js
@@ -5,6 +5,7 @@ const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
 
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie? api_key=f69b71c8e65b1aef0270ca2280da25a6&query="`;
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -12,6 +13,41 @@ async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
   showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+          <img
+            src="${IMG_PATH + movie.poster_path}"
+            alt="${movie.title}"
+          />
+          <div class="movie-info">
+            <h3>${movie.title}</h3>
+            <span class="${getClassByRate(vote_average)}">${
+      movie.vote_average
+    }</span>
+          </div>
+          <div class="overview">
+            <h3>${movie.overview}</h3>
+          </div>
+    `;
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote > 7.5) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
 }
 
 form.addEventListener('submit', (e) => {
