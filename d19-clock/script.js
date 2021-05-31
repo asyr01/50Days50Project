65409311c6447905dc5450baf480bc1f8bd07dcb
example <alisayar0107@gmail.com>
2021-05-31 14:06:29 +0300

    12 hours calculated for 360 deg with helper function.

--- d19-clock/script.js
+++ d19-clock/script.js
@@ -41,3 +41,43 @@ toggle.addEventListener('click', (e) => {
     e.target.textContent = 'Light Mode';
   }
 });
+
+// Clock functionality
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const hours = time.getHours();
+  // since it's a 12 hours clock.
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg) `;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg) `;
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg) `;
+}
+
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+setTime();
