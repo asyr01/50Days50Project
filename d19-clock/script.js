0ae99fa5613a493e1f5863e0a748d0d6e1ed70de
example <alisayar0107@gmail.com>
2021-05-31 13:41:05 +0300

    dark/light mode functionality added

--- d19-clock/script.js
+++ d19-clock/script.js
@@ -4,3 +4,40 @@ const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+// Light Dark Mode Functionality
+toggle.addEventListener('click', (e) => {
+  e.preventDefault();
+  const html = document.querySelector('html');
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
+    e.target.textContent = 'Dark Mode';
+  } else {
+    html.classList.add('dark');
+    e.target.textContent = 'Light Mode';
+  }
+});
