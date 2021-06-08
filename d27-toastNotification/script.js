e620311974cf21a55ab19a70bb1b1c016ea03f79
example <alisayar0107@gmail.com>
2021-06-08 10:27:33 +0300

    arguments used to specify notification

--- d27-toastNotification/script.js
+++ d27-toastNotification/script.js
@@ -1,5 +1,5 @@
-const button = document.getElementsById('button');
-const toasts = document.getElementsById('toasts');
+const button = document.getElementById('button');
+const toasts = document.getElementById('toasts');
 
 // To check random messages
 const messages = [
@@ -11,8 +11,25 @@ const messages = [
 
 button.addEventListener('click', () => createNotification());
 
-function createNotification() {
-  messages.forEach((message) => {
-    const message = document.createElement('div');
-  });
+// This way you push one notification, we can use forEach to push all notifications at once
+// Close element for closing notification we can make it like popup by not appending close element
+// This type element can specified as error success like in bootstrap
+function createNotification(message = null, type = null) {
+  const notif = document.createElement('div');
+  const close = document.createElement('p');
+  notif.classList.add('toast');
+  notif.classList.add(type ? type : 'info');
+  close.classList.add('close');
+  close.innerText = 'X';
+  notif.innerText = message ? message : getRandomMessage();
+
+  notif.appendChild(close);
+  toasts.appendChild(notif);
+
+  close.addEventListener('click', () => setTimeout(notif.remove(), 300));
+}
+
+// To get a random index of array
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
 }
