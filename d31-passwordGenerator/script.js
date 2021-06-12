525deeca4fa80fc8712e59376c607c00f29589c3
example <alisayar0107@gmail.com>
2021-06-12 19:42:27 +0300

    password generated by script

--- d31-passwordGenerator/script.js
+++ d31-passwordGenerator/script.js
@@ -1,3 +1,61 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+// Checks settings and calls the generatePassword function
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// Function which returns a password
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  // They have false or true values in them
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    // Returns only true
+    (item) => Object.values(item)[0]
+  );
+  // If nothing checked in the settings return nothing
+  if (typesCount === 0) {
+    return '';
+  }
+  // Generate password by getting name and and calling it
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      // Call the function and append to the generatedPassord
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword;
+  return finalPassword;
+}
+
+// An object that keeps random functions
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
 // Gives a random lower-letter from ASCII Table 97-122
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -13,10 +71,8 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-// Get a random symbol from the string
+// Gives a random symbol from the string
 function getRandomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,"+';
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(getRandomSymbol());
