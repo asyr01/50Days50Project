30d0529a2ef70c96a6610b953185992cfca1b9e0
example <alisayar0107@gmail.com>
2021-06-14 15:23:49 +0300

    localStorage functionality added, project completed

--- d33-notesAPP/script.js
+++ d33-notesAPP/script.js
@@ -1,9 +1,16 @@
 const addBtn = document.getElementById('add');
+// Get data from local storage
+const notes = JSON.parse(localStorage.getItem('notes'));
 
 addBtn.addEventListener('click', () => addNewNote(''));
 
+// If there is data on local storage
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
+
 function addNewNote(text) {
-  // Creates a new node
+  // Creates a new note
   const note = document.createElement('div');
   note.classList.add('note');
   note.innerHTML = `
@@ -18,20 +25,40 @@ function addNewNote(text) {
   const deleteBtn = note.querySelector('.trash');
   const main = note.querySelector('.main');
   const textArea = note.querySelector('textarea');
-
+  // Set the textarea value to the div
   textArea.value = text;
   main.innerHTML = marked(text);
 
-  deleteBtn.addEventListener('click', () => note.remove());
+  // Delete the note
+  deleteBtn.addEventListener('click', () => {
+    note.remove();
+    updateLS();
+  });
 
+  // Switch between textarea and div
   editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
     textArea.classList.toggle('hidden');
   });
 
+  // When there is a change updateLS and use markedown
   textArea.addEventListener('input', (e) => {
     main.innerHTML = marked(e.target.value);
+    updateLS();
   });
 
+  // Append the note to the body
   document.body.appendChild(note);
+
+  // To scroll bottom when new note added
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+// Saving notes to the local storage
+function updateLS() {
+  const noteTexts = document.querySelectorAll('textarea');
+  const notes = [];
+  // Take the values of the textareas to the array
+  noteTexts.forEach((note) => notes.push(note.value));
+  localStorage.setItem('notes', JSON.stringify(notes));
 }
