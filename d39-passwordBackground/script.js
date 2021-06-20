6ac27f0f5d74473fe4e994a16a511a8c18ad7ee7
example <alisayar0107@gmail.com>
2021-06-20 13:37:05 +0300

    project completed

--- d39-passwordBackground/script.js
+++ d39-passwordBackground/script.js
@@ -1,9 +1,9 @@
 const password = document.getElementById('password');
 const backgroundEl = document.getElementById('background');
+
 password.addEventListener('input', (e) => {
   const input = e.target.value;
   const length = input.length;
-  const blurVal = 20 - length * 2;
+  let blurVal = 20 - length * 2;
   if (blurVal > 0) backgroundEl.style.filter = `blur(${blurVal}px)`;
-  console.log(blurVal);
 });
