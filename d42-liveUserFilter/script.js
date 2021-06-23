b0f2aff23c2d2d8fd8d89fed1fa304740d77421b
example <alisayar0107@gmail.com>
2021-06-23 18:22:42 +0300

    script completed, project completed

--- d42-liveUserFilter/script.js
+++ d42-liveUserFilter/script.js
@@ -13,19 +13,37 @@ async function getData() {
 
   // Clear results
   result.innerHTML = '';
+
   // results which came from api, destructuring applied at the above
   results.forEach((user) => {
-    let country = user.location.country;
-    let countryCAP = country.toUpperCase();
+    let country = user.location.country.toUpperCase();
     const li = document.createElement('li');
     listItems.push(li);
     li.innerHTML = `
     <img src="${user.picture.large}" alt="${user.name.first}">
     <div class="user-info">
      <h4>${user.name.first} ${user.name.last}</h4>
-     <p>${user.location.city}, ${countryCAP}</p>
+     <p>${user.location.city}, ${country}</p>
      </div>
     `;
     result.appendChild(li);
   });
 }
+
+// Fİlter when there is a input
+filter.addEventListener('input', (e) => {
+  filterData(e.target.value);
+});
+
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    // Check if listItems array includes input, hide or show by hide class
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove('hide');
+    } else {
+      item.classList.add('hide');
+    }
+  });
+}
+
+filterData();
