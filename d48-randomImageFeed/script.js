02c7a84f9cd2eeadd02540b925ae09c5d95e236c
example <alisayar0107@gmail.com>
2021-06-29 05:52:37 +0300

    functions created to get random images

---
+++ d48-randomImageFeed/script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+const unsplashURL = 'https://source.unsplash.com/random/';
+const rows = 10;
+
+// To get images -each row includes three images-
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement('img');
+  img.src = `${unsplashURL}`;
+}
+
+// Get different size images
+function getRandomSize() {
+  return `${getRandomNr()}x${getRandomNr()}`;
+}
+
+// Get random number to get different size images
+function getRandomNr() {
+  return Math.floor(Math.random() * 10 + 300);
+}
