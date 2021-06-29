5fe4c0a91d5cff4ef67981e62d6383f32ba11386
example <alisayar0107@gmail.com>
2021-06-29 06:03:50 +0300

    styling completed

--- d48-randomImageFeed/script.js
+++ d48-randomImageFeed/script.js
@@ -2,10 +2,11 @@ const container = document.querySelector('.container');
 const unsplashURL = 'https://source.unsplash.com/random/';
 const rows = 10;
 
-// To get images -each row includes three images-
+// To get images -each row includes three images- and insert them to DOM
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement('img');
-  img.src = `${unsplashURL}`;
+  img.src = `${unsplashURL}${getRandomSize()}`;
+  container.appendChild(img);
 }
 
 // Get different size images
