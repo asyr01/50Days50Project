e29cfc31d0c6d4e054afed5f441cd388179f59d3
example <alisayar0107@gmail.com>
2021-06-30 15:43:24 +0300

    first commit of the template

--- d48-randomImageFeed/script.js
+++ d48-randomImageFeed/script.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container');
 const unsplashURL = 'https://source.unsplash.com/random/';
 const rows = 10;
 
-// To get images -each row includes three images- and insert them to DOM
+// To create images -each row includes three images- and insert them to DOM
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement('img');
   img.src = `${unsplashURL}${getRandomSize()}`;
