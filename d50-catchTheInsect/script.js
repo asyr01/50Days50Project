35e9251c6498f1ee1efd203cf1f07a9855b13592
example <alisayar0107@gmail.com>
2021-07-02 06:28:16 +0300

    script completed, project completed

--- d50-catchTheInsect/script.js
+++ d50-catchTheInsect/script.js
@@ -27,6 +27,21 @@ choose_insect_btn.forEach((btn) => {
   });
 });
 
+// Sets an interval to start counting time etc.
+function startGame() {
+  setInterval(increaseTime, 1000);
+}
+
+// Starts time and insert a 0 according to condition.
+function increaseTime() {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  timeEl.innerHTML = `Time: ${m}:${s}`;
+  seconds++;
+}
+
 // Inserts insect to the DOM, top and left values are random.
 function createInsect() {
   const insect = document.createElement('div');
@@ -51,6 +66,25 @@ function getRandomLocation() {
   return { x, y };
 }
 
+// When you clicked on insect it runs
 function catchInsect() {
-  console.log(123);
+  increaseScore();
+  this.classList.add('caught');
+  setTimeout(() => this.remove(), 2000);
+  addInsects();
+}
+
+// Add more insects to the game
+function addInsects() {
+  setTimeout(createInsect, 1000);
+  setTimeout(createInsect, 1500);
+}
+
+// increase the score when clicked display annoyed message after 19.
+function increaseScore() {
+  score++;
+  if (score > 19) {
+    message.classList.add('visible');
+  }
+  scoreEl.innerHTML = `Score: ${score}`;
 }
