e6464ac05172fcf6c72cf619ab6e81677cd51afa
example <alisayar0107@gmail.com>
2021-07-05 06:36:31 +0300

    project revisited, input added to write custom sentences

--- d30-autoTextEffect/script.js
+++ d30-autoTextEffect/script.js
@@ -1,15 +1,26 @@
 const textEl = document.querySelector('#text');
 const speedEl = document.querySelector('#speed');
-const input = document.querySelector('#input');
-
-const text = 'We love programming';
+const inputEl = document.querySelector('#input');
+const startBtn = document.querySelector('#start-btn');
 
 // index to increase text when inserted, speed for typing effect 600 could be any value.
 let idx = 1;
 let speed = 600 / speedEl.value;
 
-writeText();
+// Default value of text
+let text = 'We Love Programming!';
 
+// Reads text from input  to determine text value
+function readText() {
+  if (inputEl.value) {
+    text = inputEl.value;
+  }
+  inputEl.style.display = 'none';
+  startBtn.style.display = 'none';
+  return text;
+}
+
+// Writes text with a managable speed
 function writeText() {
   textEl.innerText = text.slice(0, idx);
   idx++;
@@ -26,7 +37,6 @@ speedEl.addEventListener('input', (e) => {
   speed = 600 / e.target.value;
 });
 
-// To figure out the text given with input.
-input.addEventListener('submit', () => {
-  console.log('ali');
-});
+// Event listeners to read and write text
+startBtn.addEventListener('click', readText);
+startBtn.addEventListener('click', writeText);
